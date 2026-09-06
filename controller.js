5ba77c167e4283b61175ed207a0f4cda180559a8
Joseff-Marvell <jmarvell@ukr.net>
2026-09-06 12:34:35 +0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ exports.main = (req, res) => {
 	const Datastore = require('nedb')
 	const db = new Datastore({ filename: 'lvivold.json', autoload: true })
 	db.find({ table:"arctype" }, (err, docs) => {
-		var ilen = docs.length()
+		var ilen = docs.length
 		res.render(path + '/index.ejs', { types: docs, len: ilen })
 	})	
 }
